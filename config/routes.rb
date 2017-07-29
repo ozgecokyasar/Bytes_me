7dc44d04b71f26d3536a1ec65ac83bf1347ca219
jangroy <jangroy@hotmail.com>
2017-07-28 17:19:40 -0700

    -add sessions view -add users view -add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-  get('/', { to: 'welcome#about' })
+  get('/', { to: 'welcome#about', as: 'home' })
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :users, only: [:new, :create]
+
+  resources :sessions, only: [:new, :create] do
+    delete :destroy, on: :collection
+  end
+
 end
