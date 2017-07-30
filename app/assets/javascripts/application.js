c09e0abeb765130b21f0e6abb4df8ea28bde2ef4
Ozge Cokyasar <ozgecokyasar@gmail.com>
2017-07-30 15:29:14 -0700

    sidenav

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -25,4 +25,12 @@ $(document).ready(() => {
       .map(x => x.checked = false)
     })
   })
+
+  $('.button-collapse').sideNav({
+     menuWidth: 300, // Default is 300
+     edge: 'right', // Choose the horizontal origin
+     closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+     draggable: true, // Choose whether you can drag to open on touch screens,
+
+   })
 })
