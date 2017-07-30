be293220e56cbdf74b780ff968195c39440a994a
Samuel Gebrehiwet <samuelbruk@gmail.com>
2017-07-29 17:16:36 -0700

    add sign up confirmation

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   get('/', { to: 'welcome#about', as: 'home' })
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create] do
+    member do
+      get :confirm_email
+    end
+  end
 
   resources :sessions, only: [:new, :create] do
     delete :destroy, on: :collection
